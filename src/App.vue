6893ed050c8d81c8c89e43784725e076ca2d0d5f
Radih Paraguas Jr <radih@besnard-consulting.com>
2023-07-23 11:14:50 +0800

    added dummy printable call number

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
 
 <template>
   <div>
-      <router-link to="/">Go to Home</router-link>
-      <router-link to="/print-call-number">Go to Print Call Number</router-link>
+      <div><router-link to="/">Go to Home</router-link></div>
+      <div><router-link to="/print-call-number">Go to Print Call Number</router-link></div>
       <router-view></router-view>
   </div>
 </template>
