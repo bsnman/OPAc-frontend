cf09072c4aac524d4b2f1c0cdad6587875e47aaf
Radih Paraguas Jr <radih@besnard-consulting.com>
2023-08-13 18:17:59 +0800

    added base structure for custom css for common components

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import "./style.css";
 import "./font.css";
+import "./assets/css/index.scss";
 import App from "./App.vue";
 import router from "./router.ts";
 import pinia from "./store";
