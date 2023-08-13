9af63fdaf162cff6e8deae9fad83fdc7789064d9
Radih Paraguas Jr <radih@besnard-consulting.com>
2023-08-13 18:30:19 +0800

    add nav bar header

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <script setup lang="ts">
 import SideBar from "./components/common/SideBar.vue";
+import NavBar from "./components/common/NavBar.vue";
 </script>
 
 <template>
   <div class="main-container">
-    <div class="sidebar-container">
-      <SideBar />
+    <div class="navbar-container">
+      <NavBar />
     </div>
-    <div class="workspace-container">
-      <router-view></router-view>
+
+    <div style="display: flex">
+      <div class="sidebar-container">
+        <SideBar />
+      </div>
+      <div class="workspace-container">
+        <router-view></router-view>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
 .main-container {
-  display: flex;
+  width: 100vw;
   height: 100%;
 }
 .sidebar-container {
---
+++ src/components/common/NavBar.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="navbar-component">
+    <div>
+      <div class="app-title">Julie Amores OPAC</div>
+    </div>
+    <div class="app-router-links">
+      <div><router-link to="/">Home</router-link></div>
+      <div>
+        <router-link to="/print-call-number">Dashboard</router-link>
+      </div>
+      <div>
+        <router-link to="/print-call-number">Catalog</router-link>
+      </div>
+      <div>
+        <router-link to="/reports">Reports</router-link>
+      </div>
+      <div>
+        <router-link to="/reports">Admin</router-link>
+      </div>
+      <div>
+        <router-link to="/reports">Settings</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.navbar-component {
+  height: calc(28px + 2rem);
+  display: flex;
+  border-bottom: 1px solid #aaa;
+}
+
+.app-title {
+  padding: 1rem;
+  font-size: 1.2em;
+  font-weight: bold;
+  border-right: 1px solid #aaa;
+}
+
+.app-router-links {
+  padding: 1rem;
+  display: flex;
+
+  > div > a {
+    margin: 0 1rem;
+  }
+}
+</style>
